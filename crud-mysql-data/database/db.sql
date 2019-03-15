8cd4dfaad2592c07e293cd73be7cf24305b47dc8
darkunchain <luisfer74081@gmail.com>
2019-03-15 15:58:00 -0500

    datos ok

---
+++ crud-mysql-data/database/db.sql
@@ -0,0 +1,20 @@
+-- to create a new database
+CREATE DATABASE nodejsdata;
+
+-- to use database
+use nodejsdata;
+
+-- creating a new table
+CREATE TABLE datos (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  label VARCHAR(100) NOT NULL,
+  valor VARCHAR(10) NOT NULL
+);
+
+-- to show all tables
+show tables;
+
+-- to describe table
+describe datos;
+
+
